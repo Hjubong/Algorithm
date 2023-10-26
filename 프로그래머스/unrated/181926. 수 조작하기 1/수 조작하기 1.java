806fbe61d625a0286a452eb28cc2ecb643bc3cd9
Hjubong <138987755+Hjubong@users.noreply.github.com>
2023-10-26 09:45:36 +0900

    [level unrated] Title: 수 조작하기 1, Time: 1.18 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int n, String control) {
+        int answer = 0;
+        
+        for(int i=0; i<control.length(); i++){
+            char command = control.charAt(i);
+            
+            if(command == 'w'){
+                n += 1;
+            } else if(command == 's'){
+                n -= 1;
+            } else if(command == 'd'){
+                n += 10;
+            } else if(command == 'a'){
+                n -= 10;
+            }
+        }
+        
+        
+        return n;
+    }
+}
